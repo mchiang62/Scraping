97f7e9cc43d39be83789ed4ba096c14ab549b3d5
Melissa Chiang <mchiang62@gmail.com>
2020-01-20 17:59:13 -0500

    added some public files

--- server.js
+++ server.js
@@ -16,7 +16,22 @@ var PORT = 3000;
 // Initialize Express
 var app = express();
 
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
+
+
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
+  
