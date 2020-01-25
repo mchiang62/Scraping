8a6bc8bdb318e651d7f862cda85d6a60eff39744
Melissa Chiang <mchiang62@gmail.com>
2020-01-25 17:53:42 -0500

    scraped business insider

---
+++ public/app.js
@@ -0,0 +1,18 @@
+// Grab the articles as a json
+$.getJSON("/articles", function(data) {
+    // For each one
+    for (var i = 0; i < data.length; i++) {
+      // Display the apropos information on the page
+      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + "www.businessinsider.com" + data[i].link + "</p>");
+    }
+  });
+
+  $(".save").on("click", function() {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "POST",
+        url: "/articles/save/" + thisId
+    }).done(function(data) {
+        window.location = "/"
+    })
+});
